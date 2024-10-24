6906ab863b6bf7af9a9a17c618d29aded16a9ee5
adelhassan1 <tomasander12@gmail.com>
2024-10-24 06:48:09 +0300

    async awiat

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Execute multiple coroutines at the same time with async.
+"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    Spawn wait_random n times with the specified max_delay.
+    """
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        if not delays:
+            delays.append(delay)
+        else:
+            for i in range(len(delays)):
+                if delay < delays[i]:
+                    delays.insert(i, delay)
+                    break
+            else:
+                delays.append(delay)
+    return delays
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Tasks
+"""
+
+
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list:
+    """
+    Spawn task_wait_random n times with the specified max_delay.
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        if not delays:
+            delays.append(delay)
+        else:
+            for i in range(len(delays)):
+                if delay < delays[i]:
+                    delays.insert(i, delay)
+                    break
+            else:
+                delays.append(delay)
+    return delays
